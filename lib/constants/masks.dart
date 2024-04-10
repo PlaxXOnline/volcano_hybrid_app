b9265e847f630c366658ae6f283e5b0f2620bd0e
Janik Kahle <Janik_K@gmx.de>
2024-04-10 20:19:13 +0200

    added constants, uuids, temperature and masks

---
+++ lib/constants/masks.dart
@@ -0,0 +1,9 @@
+const int heatingMask = 0x0020;
+const int fanMask = 0x2000;
+
+const int displayOnCoolingMask = 0x1000;
+const int displayOffCoolingMask = 0x10000 + displayOnCoolingMask;
+const int celciusMask = 0x200;
+const int fahrenheitMask = 0x10000 + celciusMask;
+const int vibrationOnMask = 0x400;
+const int vibrationOffMask = 0x10000 + vibrationOnMask;
